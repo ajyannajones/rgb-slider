aa3bc362925bcdc3faf4604bc60e0e80d3d4e1e8
Ajyanna Jones <ajones@wlapcs.org>
2018-01-24 19:08:15 +0000

    rbg slider

--- sketch.js
+++ sketch.js
@@ -1,8 +1,28 @@
+var rickya, grape, billie;
+
 function setup(){
     createCanvas(600,400);
     background(0);
+    
+   rickya = createSlider(0, 255, 125);
+   rickya.position(20, 20);
+   
+   grape = createSlider(0, 255, 125);
+   grape.position(20, 60);
+   
+   billie = createSlider(0, 255, 125);
+   billie.position(20, 100);
 }
 
 function draw(){
-    
+   rv = rickya.value();
+   gv = grape.value();
+   bv = billie.value();
+   
+   background(rv, gv, bv);
+   
+   textSize(30);
+   textAlign(CENTER, CENTER);
+   message = "RGB Slider";
+   text(message, width/2, height/2);
 }
